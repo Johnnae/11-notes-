6c4834c86e23fd74108a7f5ca74388497364c5c7
JaeBEE <johnnae908@yahoo.com>
2023-09-29 11:23:58 -0700

    edir htmlroutes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,14 @@
 const Router = require("express").Router();
 
 const path = require('path') 
-
+//sends index.html as response
 Router.get( '/', (req,res)=> {
-    res.sendfile(path.join)
-})
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+    
+  // sends notes.html as response
+Router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+module.exports = Router;
